b9f24a21600d1316417c76e218aa5349052a7e71
Abhishek <abfeb8@gmail.com>
2020-07-27 19:36:15 +0530

    updated tradeConfirmation function to handle #N/A error from google sheet

--- Trading/main.py
+++ Trading/main.py
@@ -4,11 +4,11 @@ sheet = Connect("Weekly Buy-Sell", "Clean")
 names = sheet.row_values(1)
 
 
-def filterData(a, b):
+def filterData(a, b):  # will return a list of dictionary containg rows to be processed
     # a, b is the range of rows which need to be processed
-    # this function will return a list of dictionarys with col names as keys and row as values, eg:
-    '''{'Timestamp': '12/07/2020', 'Call': 'Long', 'Stock Name': 'ULTRACEMCO', 'Stop Loss': '3740', 'Price': '3790', 
-    'Target': '4030', 'SL%': '1.32%', 'Target%': '6.33%', 'Risk/Reward': '0.21', 'Live': '3870', 'Dlow': '3781', 
+    # this is an examle of an element of the list
+    '''{'Timestamp': '12/07/2020', 'Call': 'Long', 'Stock Name': 'ULTRACEMCO', 'Stop Loss': '3740', 'Price': '3790',
+    'Target': '4030', 'SL%': '1.32%', 'Target%': '6.33%', 'Risk/Reward': '0.21', 'Live': '3870', 'Dlow': '3781',
     'DHigh': '3901', 'Trade': '', 'Status': '', 'Trade_LTP': '3779.5', 'Trade_Low': '3735', 'Trade_High': '3869'}'''
     # for every row there will be one dictionary element present
     mix = []
@@ -21,27 +21,34 @@ def filterData(a, b):
     return mix
 
 
-def tradeConfirmation(row):
+def tradeConfirmation(row):  # check wethere a trade has been taken or not
     if row["Call"] == "Long":
-        if row["Trade_Low"] <= row["Price"]:
-            row["Trade"] = 'In'
-            return True
-        else:
-            row['Trade'] = 'Out'
-            return False
-    else:
-        # short position
-        if row["Call"] == "Short":
-            if row["Trade_Low"] >= row["Price"]:
+        try:
+            if float(row["Trade_Low"]) <= float(row["Price"]):
                 row["Trade"] = 'In'
                 return True
             else:
                 row['Trade'] = 'Out'
                 return False
-            pass
+        except Exception:
+            print(">>Error in ", row["Stock Name"])
+
+    else:
+        # short position
+        if row["Call"] == "Short":
+            try:
+                if float(row["Trade_Low"]) >= float(row["Price"]):
+                    row["Trade"] = 'In'
+                    return True
+                else:
+                    row['Trade'] = 'Out'
+                    return False
+            except Exception:
+                print(">>Error in ", row["Stock Name"])
 
 
 rows = filterData(2, 19)
 
-tradeConfirmation(rows[0])
-print(rows[0])
+for i in rows:
+    tradeConfirmation(i)
+    # print(i["Stock Name"], i["Trade"])
